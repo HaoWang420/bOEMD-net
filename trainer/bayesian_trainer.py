afd909935788d18f5f81ce33aea643e71674f53b
WinstonHuTiger <2953620996@qq.com>
2021-12-01 16:05:46 +0800

    add multi-sample evaluation function

--- trainer/bayesian_trainer.py
+++ trainer/bayesian_trainer.py
@@ -50,82 +50,52 @@ class BayesianTrainer(Trainer):
         
         return output, kl, loss
     
-    # TODO
+
     # multi-sample evaluation
+    def val(self, epoch):
+        self.model.eval()
+        self.evaluator.reset()
+        tbar = tqdm(self.val_loader, desc='\r')
+        for i, sample in enumerate(tbar):
+            if self.args.dataset == 'lidc-syn-rand':
+                image, target = sample['image'], sample['labels']
+            else:
+                image, target = sample['image'], sample['label']
+            n, c, w, h = target.shape
+            if self.args.cuda:
+                image, target = image.cuda(), target.cuda()
+            kl_losses = torch.zeros((self.num_sample, n))
 
-    def get_weight_SNR(self):
-        weight_SNR_vec = []
+            assert image.shape[0] == 1
+            if self.args.dataset == 'lidc-syn-rand':
+                image = image.repeat(self.num_sample * 3, 1, 1, 1)
+            else:
+                image = image.repeat(self.num_sample, 1, 1, 1)
 
-        for module in self.model.modules():
-            if hasattr(module, 'kl_loss'):
+            with torch.no_grad():
+                predictions, kl = self.model(image)
 
-                W_mu = module.W_mu.data
-                W_p = module.W_rho.data
-                sig_W = 1e-6 + F.softplus(W_p, beta=1, threshold=20)
+            if self.args.dataset == 'lidc-syn-rand':
+                predictions = predictions.reshape((self.num_sample, 3, predictions.shape[2], predictions.shape[3]))
 
-                b_mu = module.bias_mu.data
-                b_p = module.bias_rho.data
-                sig_b = 1e-6 + F.softplus(b_p, beta=1, threshold=20)
+            mean_out = torch.mean(predictions, dim=0, keepdim=True)
+            mean_kl_loss = torch.mean(kl_losses)
 
-                W_snr = (torch.abs(W_mu) / sig_W)
-                b_snr = (torch.abs(b_mu) / sig_b)
+            self.evaluator.add_batch(target, pred)
 
-                for weight_SNR in W_snr.cpu().view(-1):
-                    weight_SNR_vec.append(weight_SNR)
+        results = self.evaluator.compute()
 
-                for weight_SNR in b_snr.cpu().view(-1):
-                    weight_SNR_vec.append(weight_SNR)
+        for metric in results:
+            self.writer.add_scalar(metric, results[metric], epoch)
 
-        return np.array(weight_SNR_vec)
+        for metric in results:
+            print(f"{metric} {results[metric]}")
 
-    def sample_weights(self, W_mu, b_mu, W_p, b_p):
-
-        eps_W = W_mu.data.new(W_mu.size()).normal_()
-        # sample parameters
-        std_w = 1e-6 + F.softplus(W_p, beta=1, threshold=20)
-        W = W_mu + 1 * std_w * eps_W
-
-        if b_mu is not None:
-            std_b = 1e-6 + F.softplus(b_p, beta=1, threshold=20)
-            eps_b = b_mu.data.new(b_mu.size()).normal_()
-            b = b_mu + 1 * std_b * eps_b
-        else:
-            b = None
-
-        return W, b
-
-    def get_weight_KLD(self, Nsamples=20):
-        weight_KLD_vec = []
-
-        for module in self.model.modules():
-            if hasattr(module, 'kl_loss'):
-
-                W_mu = module.W_mu.data
-                W_p = module.W_rho.data
-
-                b_mu = module.bias_mu.data
-                b_p = module.bias_rho.data
-
-                std_w = 1e-6 + F.softplus(W_p, beta=1, threshold=20)
-                std_b = 1e-6 + F.softplus(b_p, beta=1, threshold=20)
-
-                KL_W = W_mu.new(W_mu.size()).zero_()
-                KL_b = b_mu.new(b_mu.size()).zero_()
-                for i in range(Nsamples):
-                    W, b = self.sample_weights(W_mu=W_mu, b_mu=b_mu, W_p=W_p, b_p=b_p)
-                    # Note that this will currently not work with slab and spike prior
-                    KL_W += metrics.isotropic_gauss_loglike(W, W_mu, std_w, do_sum=False) - module.likelihood.loglike(W,
-                                                                                                                      do_sum=False)
-                    KL_b += metrics.isotropic_gauss_loglike(b, b_mu, std_b, do_sum=False) - module.likelihood.loglike(b,
-                                                                                                                      do_sum=False)
-
-                KL_W /= Nsamples
-                KL_b /= Nsamples
-
-                for weight_KLD in KL_W.cpu().view(-1):
-                    weight_KLD_vec.append(weight_KLD)
-
-                for weight_KLD in KL_b.cpu().view(-1):
-                    weight_KLD_vec.append(weight_KLD)
-
-        return np.array(weight_KLD_vec)
+        is_best = True
+        self.best_pred = results['qubiq']
+        self.saver.save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': self.model.module.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'best_pred': self.best_pred,
+        }, is_best)
