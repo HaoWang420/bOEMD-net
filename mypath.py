5451def3b17e364cbf016990f13315ccbb600d76
WinstonHuTiger <2953620996@qq.com>
2023-06-22 07:25:57 +0800

    add liver dataset to my path

--- mypath.py
+++ mypath.py
@@ -2,14 +2,16 @@ class Path(object):
     @staticmethod
     def getPath(dataset):
         if dataset == 'QUBIQ':
-            return r'/data/ssd/wanghao/datasets/QUBIQ'
+            return r'/data/sdb/wanghao/datasets/QUBIQ'
         if dataset == 'uncertain-brats':
             return r''
         if dataset == 'brats':
             return '/home/cvip/data/wanghao/datasets/braTS_jpg'
         if dataset == 'lidc':
-            return '/data/ssd/wanghao/datasets/'
+            return '/data/sdb/wanghao/datasets/'
         if dataset == 'lidc-patient':
-            return "/data/ssd/datasets/processed_LIDC/"
+            return "/data/sdb/datasets/processed_LIDC/"
+        if dataset == "liver":
+            return "/data/sdb/qingqiao/workspaces/datasets/liver_datasets/processed"
         else:
             raise NotImplementedError
